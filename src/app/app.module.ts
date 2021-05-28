e742a97d6ee1590c31acab7b275e3615e5677ffc
George Petrakis <georgepetrakis99@gmail.com>
2021-05-28 16:11:06 +0300

    finally got nebular working...

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { NbButtonModule, NbStatusService } from '@nebular/theme';
+import { NbThemeModule } from '@nebular/theme';
+import { Router, RouterModule, ROUTES } from '@angular/router'; // we also need angular router for Nebular to function properly
+import { NbSidebarModule, NbLayoutModule } from '@nebular/theme';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -9,10 +12,14 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
+    NbButtonModule,
+    NbLayoutModule,
+    NbSidebarModule.forRoot(),
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NbThemeModule.forRoot({name: 'cosmic'})
   ],
-  providers: [],
+  providers: [NbStatusService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
